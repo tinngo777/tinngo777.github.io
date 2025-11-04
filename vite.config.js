66de3e5bd62f5c9d758a55bbfc882fb1442ddbb8
tinngo777 <tinpro777@gmail.com>
2025-11-03 23:09:13 -0600

    cleaning up dist

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,3 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  base: "/",
-});
+export default {
+  base: '/tinngo777.github.io/', 
+};
